819b8bbcc8045425a7a92ed6cbccdbe4f1f6e9b2
Hitesh Patel <hbp976@gmail.com>
2020-08-24 12:09:51 -0600

    add form field

--- js/dom-events-form.js
+++ js/dom-events-form.js
@@ -2,3 +2,17 @@
 
 const helloForm = document.querySelector( 'form' );
 console.log( helloForm );
+
+const nameField = document.getElementById( 'your-name' );
+console.log( nameField );
+
+const helloList = document.querySelector( '#hello-list' );
+console.log ( helloList );
+
+//Event Listerner
+
+helloForm.addEventListener( 'submit', ( event ) => {
+    event.preventDefault();
+    console.log( event );
+
+});
