c738d353a28c05bdc7c63bc1826947123f111e59
Hitesh Patel <hbp976@gmail.com>
2020-08-24 13:32:02 -0600

    append the new populated LI

--- js/dom-events-form.js
+++ js/dom-events-form.js
@@ -14,5 +14,28 @@ console.log ( helloList );
 helloForm.addEventListener( 'submit', ( event ) => {
     event.preventDefault();
     console.log( event );
+    console.log( event.target );
 
-});
+     // how do we get our value from our form field?
+
+    const name = nameField.value;
+    console.log( name );
+
+    //how do i make a new LI element?
+    const newLI = document.createElement( 'LI' );
+    console.log( newLI );
+
+    //How do i insert a valuve/text into the LI element?
+
+    newLI.textContent = `Hello, ${name}!`;
+    console.log( newLI.textContent );
+    console.log( newLI );
+
+    //how do i add the LI element to the page
+
+    helloList.appendChild( newLI );
+    
+    //
+    
+   
+    });
