81f1fef321f850d02ba4a99fe3049558fdd080de
Hitesh Patel <hbp976@gmail.com>
2020-08-26 11:18:16 -0600

    output of our astronauts data

--- js/ajax-api.js
+++ js/ajax-api.js
@@ -10,9 +10,26 @@
  fetch( 'http://api.open-notify.org/astros.json' )
     .then( response => response.json() )
 
-    .then( data => { console.log( data ); } );
+    .then( data => { 
+        console.log( data ); 
 
-    
+    console.log (data);
+
+    const people = data.people;
+    console.log( people );
+
+        const peopleUL = document.createElement( 'UL');
+        for ( const person of people )
+        {
+            const personLI = document.createElement( 'LI' );
+            personLI.textContent = `On the ${person.craft} craft, astronaut "${person.name}" is present!`;
+
+            peopleUL.appendChild( personLI );
+
+        }
+        document.body.appendChild ( peopleUL );
+    } );
+
 
 
 
